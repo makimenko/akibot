e0399e36b4793fe4b78243a062a6b2f2f24ffcde
mihails.akimenko@gmail.com <mihails.akimenko@gmail.com@feca7051-6c8f-4bed-c874-17f0fb5c5eae>
2014-10-06 19:53:23 +0000

    first distance meter command

---
+++ akibot-kiss/src/com/akibot/kiss/server/ClientMessageHandler.java
@@ -0,0 +1,42 @@
+package com.akibot.kiss.server;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.akibot.kiss.component.Component;
+import com.akibot.kiss.message.CommandMessage;
+
+public class ClientMessageHandler extends Thread {
+	static final Logger log = LogManager.getLogger(ClientMessageHandler.class.getName());
+	private Component component; 
+	private LinkedBlockingQueue<Object> messages;
+	private Client client;
+	
+	public ClientMessageHandler(Client client, LinkedBlockingQueue<Object> messages, Component component) {
+		this.client = client;
+		this.messages = messages;
+		this.component = component;
+	}
+	public void run() {
+		while (true) {
+			try {
+				Object message = messages.take();
+				log.debug("Message Received: " + message);
+				// Do some handling here...
+				if (message instanceof CommandMessage) {
+					CommandMessage commandMessage = (CommandMessage)message;
+					component.executeCommand(client, commandMessage);
+				}
+			} catch (InterruptedException e) {
+				log.warn(e.getMessage());
+				e.printStackTrace();
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
