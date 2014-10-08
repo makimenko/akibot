856b54a53f40a1fa2e25c6ef5e130c990d8e1013
mihails.akimenko@gmail.com <mihails.akimenko@gmail.com@feca7051-6c8f-4bed-c874-17f0fb5c5eae>
2014-10-08 19:30:05 +0000

    Add ClientDescription

--- akibot-kiss/src/com/akibot/kiss/server/Server.java
+++ akibot-kiss/src/com/akibot/kiss/server/Server.java
@@ -4,32 +4,36 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
+import sun.misc.Cleaner;
 
 public class Server {
 	static final Logger log = LogManager.getLogger(Server.class.getName());
-	private ArrayList<Connection> clientList;
+	private ConcurrentHashMap<ClientDescription, Connection> clientList;
 	private LinkedBlockingQueue<Object> messages;
 	private ServerSocket serverSocket;
 
 	public Server(int port) throws IOException {
-		log.info("Server starting...");		
-		clientList = new ArrayList<Connection>();
+		log.info("Server starting...");
+		clientList = new ConcurrentHashMap<ClientDescription, Connection>();
 		messages = new LinkedBlockingQueue<Object>();
 		serverSocket = new ServerSocket(port);
-		log.info("Server started");	
-		
+		log.info("Server started");
+
 		Thread accept = new Thread() {
 			public void run() {
 				while (true) {
 					try {
 						Socket s = serverSocket.accept();
 						log.debug("New connection accepted");
-						clientList.add(new Connection(s, messages));
+						ClientDescription clientDescription = new ClientDescription("X", "Y");
+						clientList.putIfAbsent(clientDescription, new Connection(s, messages));
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
@@ -50,8 +54,10 @@ public class Server {
 	}
 
 	public void sendToAll(Object message) {
-		for (Connection client : clientList)
-			client.write(message);
+		for (ConcurrentHashMap.Entry<ClientDescription, Connection> entry : clientList.entrySet()) {
+			entry.getValue().write(message);
+		}
+
 	}
 
 }
