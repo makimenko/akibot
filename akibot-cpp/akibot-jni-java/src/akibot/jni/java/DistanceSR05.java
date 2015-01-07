da36ff59d360870d71ec72beb013aa406623aca3
mihails.akimenko@gmail.com <mihails.akimenko@gmail.com@feca7051-6c8f-4bed-c874-17f0fb5c5eae>
2015-01-07 18:11:44 +0000

    work in progress

--- akibot-cpp/akibot-jni-java/src/akibot/jni/java/DistanceSR05.java
+++ akibot-cpp/akibot-jni-java/src/akibot/jni/java/DistanceSR05.java
@@ -2,6 +2,7 @@ package akibot.jni.java;
 
 public class DistanceSR05 {
 
-    private native int distanceEchoMicros(int triggerPin, int echoPin, int timeout);
+    public native int distanceEchoMicros(int triggerPin, int echoPin, int timeout);
+
 
 }
