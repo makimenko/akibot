fa7afa3609584688cffaf11b1ec1afa64f992757
mihails.akimenko@gmail.com <mihails.akimenko@gmail.com@feca7051-6c8f-4bed-c874-17f0fb5c5eae>
2015-01-08 21:16:09 +0000

    First version of Working JNI

---
+++ akibot-sandbox/src/sandbox/jni/TestDistance.java
@@ -0,0 +1,19 @@
+package sandbox.jni;
+
+import akibot.jni.java.AkibotJniLibrary;
+
+public class TestDistance {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("STARTING....");
+        AkibotJniLibrary lib = new AkibotJniLibrary();
+        lib.initialize();
+        for (int i = 0; i <= 100; i++) {
+            int cm = lib.getDistance(25, 27, 50000);
+            System.out.println("Distance is: " + cm + " cm");
+            Thread.sleep(100);
+        }
+        System.out.println("END");
+    }
+    
+}
