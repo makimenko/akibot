7572da19811e0e92899a335843705ee92a1c27ac
Akimenko <Akimenko@M90-AKIMENKO.office.ctco.lv>
2015-08-25 15:49:02 +0300

    JME3 Decommissined

--- akibot-jme3/src/com/akibot/jme3/component/message/VisualizerRequest.java
+++
@@ -1,8 +0,0 @@
-package com.akibot.jme3.component.message;
-
-import com.akibot.engine2.message.Request;
-
-public class VisualizerRequest extends Request {
-	private static final long serialVersionUID = 1L;
-
-}
