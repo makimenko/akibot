b7f418d583cd9ca520c9d451b0ac4c8287b0ab10
makimenko <mihails.akimenko@gmail.com>
2015-12-22 21:25:04 +0200

    Echolocator - ok

--- akibot-sandbox/src/sandbox/pi4j/echolocator/EcholocatorExample.java
+++ akibot-sandbox/src/sandbox/pi4j/echolocator/EcholocatorExample.java
@@ -16,18 +16,19 @@ public class EcholocatorExample {
 		config.setFrequencyCorrectionFactor(new BigDecimal("1.0578"));
 		config.setServoBasePin(1);
 		config.setServoHeadPin(0);
-		config.setServoLongTime(400000 / 1000);
+		config.setServoLongTime(400); // micro to ms
 		config.setServoMin(600);
 		config.setServoMax(2500);
-		config.setServoStepTime(3000 / 1000);
-		config.setSleepBeforeDistance(5000 / 1000);
+
+		config.setServoStepTime(1); // micro to ms
+		config.setSleepBeforeDistance(0); // micro to ms
 
 		EcholocatorDevice echolocator = new EcholocatorDevice(config);
 
 		// Request Echolocator:
 		int servoBaseFrom = 600;
 		int servoBaseTo = 2500;
-		int servoBaseStep = 100;
+		int servoBaseStep = 190;
 		int servoHeadNormal = 950;
 		boolean trustToLastPosition = true;
 		float result[] = echolocator.scanDistance(servoBaseFrom, servoBaseTo, servoBaseStep, servoHeadNormal, trustToLastPosition);
