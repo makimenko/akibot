5254e5f29fa7489fcb3ee09787000853f68a34b4
makimenko <mihails.akimenko@gmail.com>
2016-11-15 20:46:16 +0200

    Switch to GSON

--- akibot-common/src/test/java/com/akibot/common/JsonTest.java
+++ akibot-common/src/test/java/com/akibot/common/JsonTest.java
@@ -9,20 +9,18 @@ import org.junit.Test;
 
 import com.akibot.common.element.Point3D;
 import com.akibot.common.message.TestMessage;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
 
 public class JsonTest {
 
 	@Test
-	public void testJsonToBean() throws JsonParseException, JsonMappingException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
+	public void testJsonToBean() throws JsonParseException, IOException {
+		Gson gson = new Gson();
 
 		String jsonInString = "{\"stringField\" : \"hello\", \"doubleField\": \"-1.001\", \"booleanField\": \"true\", \"point\" : {\"x\":\"1\", \"y\":\"2\", \"z\":3} }";
 
-		TestMessage testMessage = mapper.readValue(jsonInString, TestMessage.class);
+		TestMessage testMessage = gson.fromJson(jsonInString, TestMessage.class);
 		assertEquals("hello", testMessage.getStringField());
 		assertEquals(-1.001, testMessage.getDoubleField(), 0.0000001);
 		assertEquals(1, testMessage.getPoint().getX(), 0.0000001);
@@ -32,8 +30,8 @@ public class JsonTest {
 	}
 
 	@Test
-	public void testBeanToJson() throws JsonParseException, JsonMappingException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
+	public void testBeanToJson() throws JsonParseException, IOException {
+		Gson gson = new Gson();
 
 		TestMessage testMessage = new TestMessage();
 		testMessage.setBooleanField(true);
@@ -41,23 +39,11 @@ public class JsonTest {
 		testMessage.setDoubleField(0.03);
 		testMessage.setPoint(new Point3D(-1.01, -2.02, -3.03));
 
-		String jsonString = mapper.writeValueAsString(testMessage);
-
-		assertEquals("{\"stringField\":\"aaa\",\"doubleField\":0.03,\"booleanField\":true,\"point\":{\"x\":-1.01,\"y\":-2.02,\"z\":-3.03}}",
+		String jsonString = gson.toJson(testMessage);
+		System.out.println(jsonString);
+		assertEquals(
+				"{\"stringField\":\"aaa\",\"doubleField\":0.03,\"booleanField\":true,\"point\":{\"z\":-3.03,\"x\":-1.01,\"y\":-2.02}}",
 				jsonString);
-	}
-
-	@Test
-	public void testJsonToPartlyBean() throws JsonParseException, JsonMappingException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-
-		String jsonInString = "{\"stringField\" : \"hello\", \"doubleField\": \"-1.001\", \"booleanField\": \"true\", \"point\" : {\"x\":\"1\", \"y\":\"2\", \"z\":3} }";
-
-		JsonNode root = mapper.readTree(jsonInString);
-		JsonNode stringFieldNode = root.get("stringField");
-
-		assertEquals("hello", stringFieldNode.textValue());
-
 	}
 
 }
